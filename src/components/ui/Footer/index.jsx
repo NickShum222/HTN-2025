c785456f407c584d4371e84b1c3ef271bdf423bb
Nick Shum <n4shum@uwaterloo.ca>
2025-02-14 11:24:06 -0500

    Update Footer

--- src/components/ui/Footer/index.jsx
+++ src/components/ui/Footer/index.jsx
@@ -3,13 +3,16 @@ import React from "react";
 const index = () => {
   return (
     <div className="w-full border-t-[1px] border-offWhite border-solid flex sm:flex-row  sm:justify-start justify-between sm:items-center pt-4 pb-8">
-      <a className="text-offWhite font-Satoshibold flex items-center md:text-[1.4em] text-[1.2em] w-1/2" href="#landing">
+      <a
+        className="text-offWhite font-Satoshibold flex items-center md:text-[1.4em] text-[1.2em] w-1/2"
+        href="#landing"
+      >
         <img
           src="./images/htn.svg"
           alt="HTN Logo"
           className="object-cover mr-2"
         />
-        Events
+        Hackathon Global Inc.
       </a>
       <a
         href="https://github.com/NickShum222/HTN-Frontend-Challenge"
